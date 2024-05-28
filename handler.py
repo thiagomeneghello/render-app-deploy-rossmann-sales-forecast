9de7fc370cd479ef9c37955457a0a3a88fd99d5a
tfmeneghello <tfmeneghello@gmail.com>
2024-05-28 00:54:13 -0300

    fix model_path

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # loading model
-model = pickle.load(open('/pickle/model_rossmann.pkl', 'rb'))
+model = pickle.load(open('pickle/model_rossmann.pkl', 'rb'))
 
 #initialize API
 app = Flask(__name__)
