0c898951ecf156352aae7933647b4f61b0020121
tfmeneghello <tfmeneghello@gmail.com>
2024-05-28 02:04:03 -0300

    nova porta:os.getenv('PORT', 10000)

--- handler.py
+++ handler.py
@@ -42,5 +42,5 @@ def rossmann_predict():
         return Response( '{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = os.getenv('PORT', 10000)
     app.run(host = '0.0.0.0', port = port)
